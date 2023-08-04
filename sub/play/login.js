c0f158503ecb66db598447afb154e0827d45c270
Gaurav Tripathi <>
2023-08-04 10:22:55 +0530

    d

--- sub/play/login.js
+++ sub/play/login.js
@@ -18,7 +18,7 @@ form.addEventListener("submit",(e)=>{
 // function for checking username and password
 
 function authentication(username,password){
-    if(username === "gaurav" && password === "gauravtripathi"){
+    if(username === "username" && password === "1235678"){
         return true
     }else{
         return false
